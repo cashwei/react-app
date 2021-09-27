45a2558f08cd03b28650859588a9d96985a38ec2
cashwei <295108054@qq.com>
2021-09-27 15:29:05 +0800

    提交测试

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
+import Header from "../../components/header";
 import './login.scss'
 
 class Login extends Component{
     render(){
         return(
             <div className="P-login">
+                <Header/>
                 <h1>Login page</h1>
             </div>
         )
